59d37a2c15073b78131134f77487cb9ef69efc31
roman <abram.room@gmail.com>
2024-05-28 01:55:27 +0300

    init backend for products and categories

--- erp-server/app/Providers/AppServiceProvider.php
+++ erp-server/app/Providers/AppServiceProvider.php
@@ -6,19 +6,10 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::except([
+            'submit'
+        ]);
     }
 }
--- erp-server/routes/web.php
+++ erp-server/routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CryptocurrencyController;
+use App\Http\Controllers\CategoriesController;
 
-
-Route::get('/api', function () {
-    return "sds";
+Route::get('/sanctum/csrf-cookie', function () {
+    return response()->json(['csrf_token' => csrf_token()]);
 });
+
+Route::get('/api/categories', [CategoriesController::class, 'index']);
+Route::post('/api/categories', [CategoriesController::class, 'store']);
 
 Route::get('/api/cryptocurrencies', [CryptocurrencyController::class, 'index']);
