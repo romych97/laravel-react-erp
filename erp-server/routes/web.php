aeb3ccda97c822e87034c73cb357691b5986dea3
roman <abram.room@gmail.com>
2024-06-10 23:36:29 +0300

    init web api rest link for products

--- erp-server/routes/web.php
+++ erp-server/routes/web.php
@@ -2,11 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CryptocurrencyController;
+use App\Http\Controllers\ProductImageController;
 use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\ProductsController;
 
 Route::get('/sanctum/csrf-cookie', function () {
     return response()->json(['csrf_token' => csrf_token()]);
 });
+
+Route::get('/api/products', [ProductsController::class, 'index']);
+Route::post('/api/products', [ProductsController::class, 'store']);
+Route::put('/api/products/{id}', [ProductsController::class, 'update']);
+Route::delete('/api/products/{id}', [ProductsController::class, 'destroy']);
+Route::post('/api/products/{id}/{imageType}', [ProductImageController::class, 'store']);
 
 Route::get('/api/categories', [CategoriesController::class, 'index']);
 Route::post('/api/categories', [CategoriesController::class, 'store']);
